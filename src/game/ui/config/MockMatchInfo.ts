aea7f7936b42954fbba2c91a08fee3bc650cea72
Yuancheng Zhang <old.far.hot@gmail.com>
2020-11-26 23:42:50 +0800

    update mock info

--- src/game/ui/config/MockMatchInfo.ts
+++ src/game/ui/config/MockMatchInfo.ts
@@ -9,8 +9,6 @@ namespace TestMode {
      * 100 => 游戏结束
      */
 
-    const uids = [101, 102, 103, 104, 105, 106];
-
     function getMatchId(isRandom = true) {
         const matchid = isRandom
             ? Math.random().toString().substr(2, 16)
@@ -19,57 +17,60 @@ namespace TestMode {
         return matchid;
     }
 
+    const uids = [101, 102, 103, 104, 105, 106];
+    const types = [0, 0, 1, 1, 1, 1]; // 1 = 机器人 0 = 正常玩家
+
+    export const MockUid: number = uids[0];
+    export const MockWdh: number = -1;
+
+    export const MockCookie: string = 'mock cookie';
+
     export const MockMatchInfo: Native.IMatchInfo = {
         status: 1, // status = 1 参考以上结果
         matchid: getMatchId(Config.RamdonMatchId), // 用后续matchId 去进行游戏结算
         players: [
             {
                 uid: uids[0],
-                type: 1, // 1 = 机器人 0 = 正常玩家
+                type: types[0],
                 avatar: 'https://endaye.com/img/icon-att-01-64x64.png',
                 mobile: '11291502685',
-                nickname: '玩家#' + uids[0],
+                nickname: `${types[0] ? '机器人' : '玩家'}#${uids[0]}`,
             },
             {
                 uid: uids[1],
-                type: 1,
+                type: types[1],
                 avatar: 'https://endaye.com/img/icon-shield-01-64x64.png',
                 mobile: '11291502685',
-                nickname: '玩家#' + uids[1],
+                nickname: `${types[1] ? '机器人' : '玩家'}#${uids[1]}`,
             },
             {
                 uid: uids[2],
-                type: 0,
+                type: types[2],
                 avatar: 'https://endaye.com/img/icon-cane-01-64x64.png',
                 mobile: '11291502685',
-                nickname: '玩家#' + uids[2],
+                nickname: `${types[2] ? '机器人' : '玩家'}#${uids[2]}`,
             },
             {
                 uid: uids[3],
-                type: 1,
+                type: types[3],
                 avatar: 'https://endaye.com/img/icon-magic-01-64x64.png',
                 mobile: '11291502685',
-                nickname: '玩家#' + uids[3],
+                nickname: `${types[3] ? '机器人' : '玩家'}#${uids[3]}`,
             },
             {
                 uid: uids[4],
-                type: 1,
+                type: types[4],
                 avatar: 'https://endaye.com/img/icon-att-01-64x64.png',
                 mobile: '11291502685',
-                nickname: '玩家#' + uids[4],
+                nickname: `${types[4] ? '机器人' : '玩家'}#${uids[4]}`,
             },
             {
                 uid: uids[5],
-                type: 1,
+                type: types[5],
                 avatar: 'https://endaye.com/img/icon-shield-01-64x64.png',
                 mobile: '11291502685',
-                nickname: '玩家#' + uids[5],
+                nickname: `${types[5] ? '机器人' : '玩家'}#${uids[5]}`,
             },
         ],
     };
-
-    export const MockUid: number = uids[2];
-    export const MockWdh: number = -1;
-
-    export const MockCookie: string = 'mock cookie';
 }
